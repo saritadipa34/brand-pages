81bcae841abfa916bdbe41923be35686c872dff3
saritadipa34 <saritadipa34@gmail.com>
2025-01-26 09:58:06 +0530

    components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+
 const App=()=>{
   return(
     <div>
-    app
+    <Header />
     </div>
   )
 }
---
+++ src/components/Body.jsx
@@ -0,0 +1,8 @@
+const Body =()=>{
+    return(
+        <div>
+            body
+        </div>
+    )
+}
+export default Body;
